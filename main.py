86c459a73c7cf0c7dbc3e3150178454e5cb2eda5
shaghayegh <valadkhani.sh@gmail.com>
2024-04-14 17:19:28 +0330

    add handlers and logics

--- main.py
+++ main.py
@@ -1,6 +1,55 @@
+import requests
 import telebot
 from decouple import config
 
 BOT_TOKEN = config('BOT_TOKEN')
 
 bot = telebot.TeleBot(BOT_TOKEN)
+
+
+@bot.message_handler(commands=['start', 'hello'])
+def send_welcome(message):
+    bot.reply_to(message, "Hello!, Wellcome")
+
+
+def get_currency_rate(from_currency: str, to_currency: str, amount: float):
+    url = "https://api.frankfurter.app/latest"
+    params = {"from": from_currency, "to": to_currency, "amount": amount}
+    response = requests.get(url, params)
+
+    return response
+
+
+@bot.message_handler(commands=['currency'])
+def from_currency_handler(message):
+    text = "which currency you want to convert from ?"
+    sent_msg = bot.send_message(message.chat.id, text)
+    bot.register_next_step_handler(sent_msg, to_currency_handler)
+
+
+def to_currency_handler(message):
+    from_currency = message.text.upper()
+    text = "which currency you want to convert to ?"
+    sent_msg = bot.send_message(message.chat.id, text)
+    bot.register_next_step_handler(sent_msg, amount_handler, from_currency)
+
+
+def amount_handler(message, from_currency):
+    to_currency = message.text.upper()
+    text = "Enter the amount you want to convert:"
+    sent_msg = bot.send_message(message.chat.id, text)
+    bot.register_next_step_handler(sent_msg, perform_conversion, from_currency, to_currency)
+
+
+def perform_conversion(message, from_currency, to_currency):
+    try:
+        amount = float(message.text)
+        conversion_result = get_currency_rate(from_currency, to_currency, amount)
+        rate = conversion_result.json()['rates'][to_currency]
+        response_message = f"{amount} {from_currency} = {rate} {to_currency}"
+        bot.send_message(message.chat.id, response_message)
+    except ValueError:
+        bot.send_message(message.chat.id, "Invalid amount. Please enter a valid number.")
+
+
+bot.infinity_polling()
